f2d16cac55c0b84584aad64249952257ac91d86c
Khuzaima <khuzaima8855.za@gmail.com>
2026-01-12 14:40:59 +0500

    minor fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,23 @@
 import { GENERAL_INFO } from '@/lib/data';
-import { GitFork, Star } from 'lucide-react';
+// import { GitFork, Star } from 'lucide-react';
 
-interface RepoStats {
-    stargazers_count: number;
-    forks_count: number;
-}
+// interface RepoStats {
+//     stargazers_count: number;
+//     forks_count: number;
+// }
 
 const Footer = async () => {
-    const repoStats = await fetch(
-        'https://api.github.com/repos/khuzaima01/portfolio-2.0',
-        {
-            next: {
-                revalidate: 60 * 60, // 1 hour
-            },
-        },
-    );
+    // const repoStats = await fetch(
+    //     'https://api.github.com/repos/khuzaima01/portfolio-2.0',
+    //     {
+    //         next: {
+    //             revalidate: 60 * 60, // 1 hour
+    //         },
+    //     },
+    // );
 
-    const { stargazers_count, forks_count } =
-        (await repoStats.json()) as RepoStats;
+    // const { stargazers_count, forks_count } =
+    //     (await repoStats.json()) as RepoStats;
 
     return (
         <footer className="text-center pb-5" id="contact">
